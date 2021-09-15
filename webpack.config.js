2571de5c84fef428334d9999a4708b2acd88b786
Ali Peyvandi <alipvd@gmail.com>
2021-09-15 14:08:04 +0430

    'create-webpack'

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',//Watch - Process and Bundel
+
+    output: {
+        //change the output path from main.js on dist to another that we want
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',// to fix mode warning in terminal
+    watch: true,
+}
