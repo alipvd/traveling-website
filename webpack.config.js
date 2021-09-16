989467b09aa9d9d39817f606d0d1241001a71f22
Ali Peyvandi <alipvd@gmail.com>
2021-09-16 20:53:31 +0430

    'add-postcss'

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-    entry: './app/assets/scripts/App.js',//Watch - Process and Bundel
+  entry: "./app/assets/scripts/App.js", //Watch - Process and Bundel
 
-    output: {
-        //change the output path from main.js on dist to another that we want
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    mode: 'development',// to fix mode warning in terminal
-    watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    }
-}
+  output: {
+    //change the output path from main.js on dist to another that we want
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app"),
+  },
+  mode: "development", // to fix mode warning in terminal
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", { loader: "postcss-loader", }],
+      },
+    ],
+  },
+};
